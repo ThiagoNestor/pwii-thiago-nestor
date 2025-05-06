08eb34be17e7085395ecdd6c460253f2ee024cab
Thiago Nestor <thiagonestorafonso@gmail.com>
2025-05-06 14:57:13 -0300

    Aula do dis 22/04

---
+++ aula03/condição.php
@@ -0,0 +1,18 @@
+<?php
+
+    $x = 5;
+    $y = 9;
+    $media = ($x + $y)/ 2;
+
+    if ($media >= 6){
+        echo "Aprovado";
+    }
+
+    elseif ($media <= 6 && $media >=4){
+        echo "Recuperação";
+    }
+
+    else ($media <= 4){
+        "Reprovado"
+    }
+?>
